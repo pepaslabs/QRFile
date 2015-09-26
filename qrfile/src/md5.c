60b98ae6c97be3d93e50c823b87fe8ce7700bef5
Jason Pepas <jasonpepas@gmail.com>
2015-09-26 18:06:44 -0500

    splitting out md5 function from main

--- qrfile/src/md5.c
+++ qrfile/src/md5.c
@@ -22,12 +22,54 @@ enum
     EXIT_FAILURE_rhash_print_bad_length2
 }; 
 
-#define MD5_FREAD_SIZE 1024
+#define MD5_FREAD_SIZE 4096
 
-int main(int argc, char *argv[])
+/* md5_rhash()
+ * - fp should be a FILE* which has already been fopen()'ed
+ * - returns 0 on success
+ */
+int md5_rhash(FILE *fp, md5_hash_t *md5)
 {
     rhash_library_init(); /* initialize static data */
 
+    rhash context;
+    {
+        context = rhash_init(RHASH_MD5);
+        if (!context) return EXIT_FAILURE_rhash_init_failed;
+    
+        char buffer[MD5_FREAD_SIZE];
+        {
+            while(!feof(fp))
+            {
+                size_t bytes_read = fread(&buffer, sizeof(char), sizeof(buffer), fp); 
+                if (ferror(fp)) return EXIT_FAILURE_fread_failed;
+
+                if (bytes_read > 0)
+                {
+                    rhash_update(context, &buffer, bytes_read);
+                }
+            }
+        }
+            
+        int result = rhash_final(context, NULL); /* finalize hash calculation */
+        // FIXME check this result
+
+        size_t bytes_printed;
+        {
+            bytes_printed = rhash_print(md5->bytes, context, RHASH_MD5, RHPR_RAW); 
+            if (bytes_printed != sizeof(md5->bytes)) return EXIT_FAILURE_rhash_print_bad_length;
+
+            bytes_printed = rhash_print(md5->hex, context, RHASH_MD5, RHPR_HEX); 
+            if (bytes_printed != sizeof(md5->hex)-1) return EXIT_FAILURE_rhash_print_bad_length2;
+        }
+    }
+    rhash_free(context);
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
     md5_hash_t checksum;
 
     FILE *fp;
@@ -35,37 +77,8 @@ int main(int argc, char *argv[])
         fp = fopen("file", "r");
         if (fp == NULL) exit(EXIT_FAILURE_fopen_failed);
         
-        rhash context;
-        {
-            context = rhash_init(RHASH_MD5);
-            if (!context) exit(EXIT_FAILURE_rhash_init_failed);
-    
-            char buffer[MD5_FREAD_SIZE];
-            {
-                while(!feof(fp))
-                {
-                    size_t bytes_read = fread(&buffer, sizeof(char), sizeof(buffer), fp); 
-                    if (ferror(fp)) exit(EXIT_FAILURE_fread_failed);
-
-                    if (bytes_read > 0)
-                    {
-                        rhash_update(context, &buffer, bytes_read);
-                    }
-                }
-            }
-            
-            int result = rhash_final(context, NULL); /* finalize hash calculation */
-
-            size_t bytes_printed;
-            {
-                bytes_printed = rhash_print(checksum.bytes, context, RHASH_MD5, RHPR_RAW); 
-                if (bytes_printed != sizeof(checksum.bytes)) exit(EXIT_FAILURE_rhash_print_bad_length);
-
-                bytes_printed = rhash_print(checksum.hex, context, RHASH_MD5, RHPR_HEX); 
-                if (bytes_printed != sizeof(checksum.hex)-1) exit(EXIT_FAILURE_rhash_print_bad_length2);
-            }
-        }
-        rhash_free(context);
+        int result = md5_rhash(fp, &checksum);
+        if (result != 0) exit(result);
     }
     if (fclose(fp) != 0) exit(EXIT_FAILURE_fclose_failed);
 
