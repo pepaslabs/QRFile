5fe742bc14c45bad236d1e73198586475f537a8f
Jason Pepas <jasonpepas@gmail.com>
2015-10-02 19:07:10 -0500

    updating build script

--- qrfile/src/build.sh
+++ qrfile/src/build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# note: you will need to run the following:
+# build dependencies:
+#
+# debian users:
 # apt-get install librhash-dev
+#
+# OS X / homebrew users:
+# brew install rhash
+# export C_INCLUDE_PATH=/usr/local/include
+# export LIBRARY_PATH=/usr/local/lib
+# export LD_LIBRARY_PATH=/usr/local/lib
 
-gcc -c hexify.c
-gcc -c md5_rhash.c
-gcc -c fsize.c
-gcc -lrhash *.o main.c
+gcc -lrhash *.c
